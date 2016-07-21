576bcbb006121412b6dba546b6ac7ea830e38ee8
valeria <ug1123@hotmail.com>
2016-07-21 00:53:36 -0500

    home page - jsp and servlet creation

---
+++ src/servlet/AppServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//@WebServlet("/AppServlet")
+@WebServlet("/")
+public class AppServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AppServlet() {
+        super();
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().println("holi :)");
+		request.getRequestDispatcher("jsp/home.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
