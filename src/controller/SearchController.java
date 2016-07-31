69bb7e5f7a2ea1008b3f6d5ff39ac93dbc0c2163
valeria <v.urquizo@pucp.pe>
2016-07-30 20:00:55 -0500

    BioPortal connection

--- src/controller/SearchController.java
+++ src/controller/SearchController.java
@@ -6,7 +6,7 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
-
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
 import model.Concept;
 
@@ -27,20 +27,55 @@ public class SearchController {
 	//demo
 	
 	public static void JenaSparqlQuery( String cad){
+		/*
 		String sparqlQueryString1 = "PREFIX dbo: <http://dbpedia.org/ontology/> " +
 		        "   SELECT ?x " +
 		        //"   FROM" +
 		        "   WHERE { " +
 		        "       <http://dbpedia.org/resource/Neuron> dbo:abstract ?x. " +
 		        "   }";
-
+		        
 		Query query = QueryFactory.create(sparqlQueryString1);
-		QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
-
+		//QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql/", query);
+		QueryEngineHTTP qexec = new QueryEngineHTTP("http://dbpedia.org/sparql/", query);
+		*/
+		
+		
+		/*
+		String sparqlQueryString1 = "PREFIX dbio: <http://data.bioontology.org/> " +
+		        "   SELECT ?label " +
+		        //"   FROM" +
+		        "   WHERE { " +
+		        "		?x dbio:prefLabel ?title " +
+		        " 		FILTER regex(?title, \"^melanoma\"). " +
+		        "   }";
+		*/
+		/*
+		String sparqlQueryString1 = "PREFIX meta: <http://bioportal.bioontology.org/metadata/def/> " +
+		        "   SELECT DISTINCT ?vrtID ?graph " +
+		        //"   FROM" +
+		        "   WHERE { " +
+		        "		?vrtID meta:hasVersion ?version . " +
+		        " 		?version meta:hasDataGraph ?graph . " +
+		        "   }";
+		*/
+		
+		String sparqlQueryString1 = "PREFIX omv: <http://omv.ontoware.org/2005/05/ontology#> "+
+							"SELECT ?ont ?name ?acr" +
+							"WHERE {"+
+							"	?ont a omv:Ontology ."+
+							"	?ont omv:acronym ?acr ."+
+							"	?ont omv:name ?name ."+
+							"}";
+			
+		Query query = QueryFactory.create(sparqlQueryString1);
+		//QueryEngineHTTP qexec = new QueryEngineHTTP("http://sparql.bioontology.org/sparql", query);		
+		QueryEngineHTTP qexec = QueryExecutionFactory.createServiceRequest("http://sparql.bioontology.org/sparql", query);
+		qexec.addParam("apikey", "8525c5a4-8bd8-4824-bb62-d3785c367f06");
 		ResultSet results = qexec.execSelect();
 		ResultSetFormatter.out(System.out, results, query);       
 
-		qexec.close() ;
+		qexec.close();
 		
 	}
 	
