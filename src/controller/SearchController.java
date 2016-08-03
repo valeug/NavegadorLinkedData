8e4f38405e5d36c3ba4e97216ca8bf1d29566fbe
valeria <v.urquizo@pucp.pe>
2016-08-02 20:37:02 -0500

    look for a term in BioPortal biomedical ontologies

--- src/controller/SearchController.java
+++ src/controller/SearchController.java
@@ -1,11 +1,16 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
 import model.Concept;
@@ -19,13 +24,98 @@ public class SearchController {
 		c.setDescription("cute thing");
 		c.setLinkedTerms(null);
 		
-		JenaSparqlQuery("Neuron");
+		//JenaSparqlQuery("Neuron");
+		//List<String>ontoList = getAllOntologies();
+		searchTerm(cad);
 		
 		return c;
 	}
 	
-	//demo
+	public static List<String> getAllOntologies(){
+		List<String> ontoList = new ArrayList<String>();
+		
 	
+		String sparqlQueryString1 = "PREFIX omv: <http://omv.ontoware.org/2005/05/ontology#> " +
+		        "   SELECT ?ont ?name ?acr ?dataGraph" +
+		        "   WHERE { " +
+		        "       ?ont a omv:Ontology; " +
+			    "   	omv:acronym ?acr; "+
+			    "   	omv:name ?name; "+
+			    "	    <http://bioportal.bioontology.org/metadata/def/hasDataGraph> ?dataGraph . " +
+				"	}"+
+			    "	LIMIT 5";
+		
+		Query query = QueryFactory.create(sparqlQueryString1);	
+		QueryEngineHTTP qexec = QueryExecutionFactory.createServiceRequest("http://sparql.bioontology.org/sparql", query);
+		qexec.addParam("apikey", "8525c5a4-8bd8-4824-bb62-d3785c367f06");
+		
+		ResultSet results = qexec.execSelect();
+		while (results.hasNext())
+		{
+			QuerySolution binding = results.nextSolution();
+			Resource subj = (Resource) binding.get("dataGraph");
+		    //System.out.println("dataGraph: " + subj.getURI());
+			ontoList.add(""+subj.getURI());
+		} 
+
+		qexec.close();
+		
+		return ontoList;
+	}
+	
+	public static void searchTerm(String term){
+		List<String>ontoList = getAllOntologies();
+		String uriS = ontolgiesGraphNames(ontoList);
+		System.out.println("uriS:");
+		System.out.println(uriS);
+		String x = "	FROM <http://bioportal.bioontology.org/ontologies/XAO> FROM <http://bioportal.bioontology.org/ontologies/ICF>";
+		String sparqlQueryString1 = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+		        "   SELECT DISTINCT *" +
+		        //uriS +
+		        //"	FROM <http://bioportal.bioontology.org/ontologies/XAO>"+
+		        //"	FROM <http://bioportal.bioontology.org/ontologies/ICF>" +
+		        uriS+
+		        "   WHERE { " +
+		        "       ?x rdfs:label ?label . " +
+			    "   	?x rdfs:subClassOf ?parent . "+
+			    "   	?parent rdfs:label ?parentLabel . "+
+			    //"	    FILTER (CONTAINS ( UCASE(str(?label)), \""+term+"\")) " +
+			    "	    FILTER (CONTAINS ( UCASE(str(?label)), \""+term.toUpperCase()+"\")) " +
+				"	}" +
+			    "LIMIT 100";
+		
+		System.out.println(sparqlQueryString1);
+		Query query = QueryFactory.create(sparqlQueryString1);	
+		QueryEngineHTTP qexec = QueryExecutionFactory.createServiceRequest("http://sparql.bioontology.org/sparql", query);
+		qexec.addParam("apikey", "8525c5a4-8bd8-4824-bb62-d3785c367f06");
+		
+		ResultSet results = qexec.execSelect();
+		ResultSetFormatter.out(System.out, results, query);     
+		/*
+		System.out.println("antes");
+		while (results.hasNext())
+		{
+			QuerySolution binding = results.nextSolution();
+			Resource subj = (Resource) binding.get("label");
+			System.out.println("1.");
+		    System.out.println("label: " + subj.getURI());
+		    
+		} 
+		System.out.println("despues");
+		*/
+		
+		qexec.close();
+	}
+	
+	public static String ontolgiesGraphNames(List<String> ontoList){
+		String uriS = "   ";
+		for(int i = 0; i<ontoList.size(); i++){
+			uriS += "FROM <" + ontoList.get(i) + "> ";
+		}
+		return uriS;
+	} 
+	
+	//demo	
 	public static void JenaSparqlQuery( String cad){
 		/*
 		String sparqlQueryString1 = "PREFIX dbo: <http://dbpedia.org/ontology/> " +
@@ -75,8 +165,6 @@ public class SearchController {
 		ResultSet results = qexec.execSelect();
 		ResultSetFormatter.out(System.out, results, query);       
 
-		qexec.close();
-		
-	}
-	
+		qexec.close();		
+	}	
 }
